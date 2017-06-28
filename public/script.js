a445591d00ea70c0db4dc1441efad46c41be97f3
bgreene2012 <ncpanther11@hotmail.com>
2017-06-27 22:35:58 -0400

    added styling to items

--- public/script.js
+++ public/script.js
@@ -1,10 +1,17 @@
 new Vue({
     el: "#app",
     data: {
-        totalItems: 0
+        totalItems: 0,
+        items: [
+            { title: "Item 1" },
+            { title: "Item 2" },
+            { title: "Item 3" }
+        ],
+        cart: [],
     },
     methods: {
-        addItems: function(){
+        addItems: function(ndx){
+            console.log(ndx);
             this.totalItems += 9.99;
         }
     }
