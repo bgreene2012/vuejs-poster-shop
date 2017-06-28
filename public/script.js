d119f3e5478423536b626c022e9b3937a263918b
bgreene2012 <ncpanther11@hotmail.com>
2017-06-27 20:12:54 -0400

    on Lecture 14

--- public/script.js
+++ public/script.js
@@ -1,1 +1,11 @@
-console.log('It works.');
+new Vue({
+    el: "#app",
+    data: {
+        totalItems: 0
+    },
+    methods: {
+        addItems: function(){
+            this.totalItems += 9.99;
+        }
+    }
+});
